9755fe4a2b6031e8b67e0ede98f86fe83ed5568b
vamshidhar70 <53972809+vamshidhar70@users.noreply.github.com>
2019-08-10 19:05:45 +0530

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class A
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int s=0,d=0,count=0;
+int x=sc.nextInt();
+int p=sc.nextInt();
+while(x!=0)
+{
+s=100-p;
+d=(s*x)/100;
+count++;
+x=s;
+
+}
+System.out.println("to get free ,no of times requried is:"+count);
+}
